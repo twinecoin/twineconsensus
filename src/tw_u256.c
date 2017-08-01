dbad175141e1d7e76813b930547bbc6a01bdd017
Tier Nolan <tier.nolan@twinecoin.org>
2017-08-01 22:25:49 +0100

    Adds multiply function for tw_u256   Set u256 to u32   Set u256 to u256   Multiply u256 by u32   Multiply u256 by u256   Tests for above

--- src/tw_u256.c
+++ src/tw_u256.c
@@ -1,5 +1,4 @@
 #include <assert.h>
-
 #include "tw_u256.h"
 
 int tw_equal(tw_u256* a, tw_u256* b) {
@@ -37,5 +36,75 @@ int tw_sub(tw_u256* y, tw_u256* a, tw_u256* b) {
   assert(borrow == 0 || borrow == -1);
 
   return borrow != 0;
+}
+
+int tw_mul_32(tw_u256* y, tw_u256* a, tw_u32 b, tw_u32 left_shift) {
+  // Unused words are zero by default
+  tw_set_32(y, 0);
+
+  // If b is zero, then use zeroed y
+  if (b == 0) {
+    return 0;
+  }
+
+  left_shift &= 7;
+  int yi = 7 - left_shift;
+  int ai = 7;
+
+  // y1 is write index and is shifted
+  tw_u64 carry = 0;
+  for (; yi >= 0; yi--) {
+    // (u32 * u32) + u32 is guaranteed to be less than max of u64
+    tw_u64 mul = ((tw_u64) a->d[ai] * (tw_u64) b) + carry;
+    y->d[yi] = (tw_u32) mul;
+    carry = mul >> 32;
+    ai--;
+  }
+
+  // If carry is non-zero, then an overflow happened
+  if (carry != 0) {
+    return 1;
+  }
+
+  // The ai variable indexes bytes in the input that weren't processed.
+  // If any are non-zero, then they would cause an overflow, since b must be
+  // non-zero by this point.
+  while (ai >= 0) {
+    if (a->d[ai] != 0) {
+      return 1;
+    }
+    ai--;
+  }
+  return 0;
+}
+
+int tw_mul(tw_u256* y, tw_u256* a, tw_u256* b) {
+ tw_u256 acc = {0, 0, 0, 0, 0, 0, 0, 0};
+ tw_u256 mul32;
+ int overflow = 0;
+
+ for (int i = 0; i < 8; i++) {
+   overflow |= tw_mul_32(&mul32, a, b->d[i], 7 - i);
+   overflow |= tw_add(&acc, &acc, &mul32);
+ }
+
+ tw_set_256(y, &acc);
+
+ assert(overflow == 0 || overflow == 1);
+
+ return overflow;
+}
+
+void tw_set_256(tw_u256* y, tw_u256* a) {
+  for (int i = 0; i < 8; i++) {
+    y->d[i] = a->d[i];
+  }
+}
+
+void tw_set_32(tw_u256* y, tw_u32 a) {
+  for (int i = 0; i < 7; i++) {
+    y->d[i] = 0;
+  }
+  y->d[7] = a;
 }
 
--- tests/test_u256.c
+++ tests/test_u256.c
@@ -6,37 +6,86 @@
 
 #include "vectors/vectors_u256.h"
 
+// u256xu256 Binary Tests
+
 START_TEST (test_tw_equal) {
   tw_u256 a, b;
-  for (int i = 0; i < U256_TEST_VECTORS_LENGTH; i++) {
-    a = u256_test_vectors[i].a;
-    b = u256_test_vectors[i].b;
-    int equal = u256_test_vectors[i].a_equal_b;
-    ck_assert_msg(tw_equal(&a, &b) == equal, "Equality check failure");
+  for (int i = 0; i < U256_TEST_VECTORS_256X256_LENGTH; i++) {
+    a = u256_test_vectors_256x256[i].a;
+    b = u256_test_vectors_256x256[i].b;
+    int equal = u256_test_vectors_256x256[i].a_equal_b;
+    ck_assert_msg(tw_equal(&a, &b) == equal, "Equality check failure for vector %d", i);
   }
 }
 END_TEST
 
 START_TEST (test_tw_add) {
   tw_u256 a, b, y;
-  for (int i = 0; i < U256_TEST_VECTORS_LENGTH; i++) {
-    a = u256_test_vectors[i].a;
-    b = u256_test_vectors[i].b;
-    int carry = u256_test_vectors[i].a_add_b_carry;
-    ck_assert_msg(tw_add(&y, &a, &b) == carry, "Carry mismatch");
-    ck_assert_msg(tw_equal(&y, &u256_test_vectors[i].a_add_b), "Addition mismatch");
+  for (int i = 0; i < U256_TEST_VECTORS_256X256_LENGTH; i++) {
+    a = u256_test_vectors_256x256[i].a;
+    b = u256_test_vectors_256x256[i].b;
+    int carry = u256_test_vectors_256x256[i].a_add_b_carry;
+    ck_assert_msg(tw_add(&y, &a, &b) == carry, "Carry mismatch for vector %d", i);
+    ck_assert_msg(tw_equal(&y, &u256_test_vectors_256x256[i].a_add_b), "Addition mismatch for vector %d", i);
   }
 }
 END_TEST
 
 START_TEST (test_tw_sub) {
   tw_u256 a, b, y;
-  for (int i = 0; i < U256_TEST_VECTORS_LENGTH; i++) {
-    a = u256_test_vectors[i].a;
-    b = u256_test_vectors[i].b;
-    int borrow = u256_test_vectors[i].a_sub_b_borrow;
-    ck_assert_msg(tw_sub(&y, &a, &b) == borrow, "Borrow mismatch");
-    ck_assert_msg(tw_equal(&y, &u256_test_vectors[i].a_sub_b), "Subtraction mismatch");
+  for (int i = 0; i < U256_TEST_VECTORS_256X256_LENGTH; i++) {
+    a = u256_test_vectors_256x256[i].a;
+    b = u256_test_vectors_256x256[i].b;
+    int borrow = u256_test_vectors_256x256[i].a_sub_b_borrow;
+    ck_assert_msg(tw_sub(&y, &a, &b) == borrow, "Borrow mismatch for vector %d", i);
+    ck_assert_msg(tw_equal(&y, &u256_test_vectors_256x256[i].a_sub_b), "Subtraction mismatch for vector %d", i);
+  }
+}
+END_TEST
+
+START_TEST (test_tw_mul) {
+  tw_u256 a, b, y;
+  for (int i = 0; i < U256_TEST_VECTORS_256X256_LENGTH; i++) {
+    a = u256_test_vectors_256x256[i].a;
+    b = u256_test_vectors_256x256[i].b;
+    int overflow = u256_test_vectors_256x256[i].a_mul_b_overflow;
+    ck_assert_msg(tw_mul(&y, &a, &b) == overflow, "Overflow mismatch for vector %d", i);
+    ck_assert_msg(tw_equal(&y, &u256_test_vectors_256x256[i].a_mul_b), "Multiplication mismatch for vector %d", i);
+  }
+}
+END_TEST
+
+// u256xu32 Binary Tests
+
+START_TEST (test_tw_mul_32) {
+  tw_u256 a, y;
+  tw_u32 b;
+  tw_u32 shift;
+  for (int i = 0; i < U256_TEST_VECTORS_256X32_LENGTH; i++) {
+    a = u256_test_vectors_256x32[i].a;
+    b = u256_test_vectors_256x32[i].b;
+    shift = u256_test_vectors_256x32[i].s;
+    int overflow = u256_test_vectors_256x32[i].a_mul_b_overflow;
+    ck_assert_msg(tw_mul_32(&y, &a, b, shift) == overflow, "Mul32 overflow mismatch for vector %d", i);
+    ck_assert_msg(tw_equal(&y, &u256_test_vectors_256x32[i].a_mul_b), "Mul32 mismatch for vector %d", i);
+  }
+}
+END_TEST
+
+// u256 Unary Tests
+
+START_TEST (test_tw_set) {
+  tw_u256 a, y;
+  tw_u256 target = {0, 0, 0, 0, 0, 0, 0, 0};
+  tw_u32 b;
+  for (int i = 0; i < U256_TEST_VECTORS_256X32_LENGTH; i++) {
+    a = u256_test_vectors_256x32[i].a;
+    b = u256_test_vectors_256x32[i].b;
+    tw_set_256(&y, &a);
+    ck_assert_msg(tw_equal(&y, &u256_test_vectors_256x32[i].a), "Set 256 mismatch for vector %d", i);
+    target.d[7] = b;
+    tw_set_32(&y, b);
+    ck_assert_msg(tw_equal(&y, &target), "Set 32 mismatch for vector %d", i);
   }
 }
 END_TEST
@@ -48,11 +97,14 @@ Suite * version_check_suite(void) {
   s = suite_create("U256 Maths Test");
 
   /* Core test case */
-  tc_core = tcase_create("Check for tw_add");
+  tc_core = tcase_create("Check for tw_u256");
 
   tcase_add_test(tc_core, test_tw_equal);
   tcase_add_test(tc_core, test_tw_add);
   tcase_add_test(tc_core, test_tw_sub);
+  tcase_add_test(tc_core, test_tw_mul_32);
+  tcase_add_test(tc_core, test_tw_mul);
+  tcase_add_test(tc_core, test_tw_set);
   suite_add_tcase(s, tc_core);
 
   return s;
