61adda23438cdc45a2271ec5c9f08b98362bd97e
Tier Nolan <tier.nolan@twinecoin.org>
2017-08-05 19:04:56 +0100

    Adds division functional and some extra functions   divide remainder   comparison (-1, 0, +1)   add u32 with shift   subtract u32 with shift   tests for above

--- src/tw_u256.c
+++ src/tw_u256.c
@@ -5,7 +5,10 @@
 #include <assert.h>
 #include "tw_u256.h"
 
-int tw_equal(tw_u256* a, tw_u256* b) {
+const tw_u256 TW_ZERO = {0, 0, 0, 0, 0, 0, 0, 0};
+const tw_u256 TW_ONE = {0, 0, 0, 0, 0, 0, 0, 1};
+
+int tw_equal(const tw_u256* a, const tw_u256* b) {
   for (int i = 7; i >= 0; i--) {
     if (a->d[i] != b->d[i]) {
       return 0;
@@ -14,7 +17,21 @@ int tw_equal(tw_u256* a, tw_u256* b) {
   return 1;
 }
 
-int tw_add(tw_u256* y, tw_u256* a, tw_u256* b) {
+int tw_compare(const tw_u256* a, const tw_u256* b) {
+  for (int i = 0; i < 8; i++) {
+    tw_u32 aw = a->d[i];
+    tw_u32 bw = b->d[i];
+    if (aw > bw) {
+      return 1;
+    }
+    if (aw < bw) {
+      return -1;
+    }
+  }
+  return 0;
+}
+
+int tw_add(tw_u256* y, const tw_u256* a, const tw_u256* b) {
   tw_u64 carry = 0;
 
   for (int i = 7; i >= 0; i--) {
@@ -28,7 +45,31 @@ int tw_add(tw_u256* y, tw_u256* a, tw_u256* b) {
   return carry != 0;
 }
 
-int tw_sub(tw_u256* y, tw_u256* a, tw_u256* b) {
+int tw_add_32(tw_u256* y, const tw_u256* a, const tw_u32 b, const tw_u32 left_shift) {
+  tw_set_256(y, a);
+
+  tw_u64 carry = 0;
+
+  int i = 7 - (left_shift & 7);
+
+  tw_u64 result = (tw_u64) a->d[i] + (tw_u64) b + carry;
+  carry = result >> 32;
+  y->d[i] = (tw_u32) result;
+
+  i--;
+
+  for (; i >= 0 && carry != 0; i--) {
+    result = (tw_u64) a->d[i] + carry;
+    carry = result >> 32;
+    y->d[i] = (tw_u32) result;
+  }
+
+  assert(carry == 0 || carry == 1);
+
+  return carry != 0;
+}
+
+int tw_sub(tw_u256* y, const tw_u256* a, const tw_u256* b) {
   tw_i64 borrow = 0;
 
   for (int i = 7; i >= 0; i--) {
@@ -42,7 +83,31 @@ int tw_sub(tw_u256* y, tw_u256* a, tw_u256* b) {
   return borrow != 0;
 }
 
-int tw_mul_32(tw_u256* y, tw_u256* a, tw_u32 b, tw_u32 left_shift) {
+int tw_sub_32(tw_u256* y, const tw_u256* a, const tw_u32 b, const tw_u32 left_shift) {
+  tw_set_256(y, a);
+
+  tw_i64 borrow = 0;
+
+  int i = 7 - (left_shift & 7);
+
+  tw_i64 result = (tw_i64) a->d[i] - (tw_i64) b + borrow;
+  borrow = result >> 32;
+  y->d[i] = (tw_u32) result;
+
+  i--;
+
+  for (; i >= 0 && borrow != 0; i--) {
+    result = (tw_i64) a->d[i] + borrow;
+    borrow = result >> 32;
+    y->d[i] = (tw_u32) result;
+  }
+
+  assert(borrow == 0 || borrow == -1);
+
+  return borrow != 0;
+}
+
+int tw_mul_32(tw_u256* y, const tw_u256* a, const tw_u32 b, const tw_u32 left_shift) {
   // Unused words are zero by default
   tw_set_32(y, 0);
 
@@ -51,8 +116,7 @@ int tw_mul_32(tw_u256* y, tw_u256* a, tw_u32 b, tw_u32 left_shift) {
     return 0;
   }
 
-  left_shift &= 7;
-  int yi = 7 - left_shift;
+  int yi = 7 - (left_shift & 7);
   int ai = 7;
 
   // y1 is write index and is shifted
@@ -82,8 +146,8 @@ int tw_mul_32(tw_u256* y, tw_u256* a, tw_u32 b, tw_u32 left_shift) {
   return 0;
 }
 
-int tw_mul(tw_u256* y, tw_u256* a, tw_u256* b) {
- tw_u256 acc = {0, 0, 0, 0, 0, 0, 0, 0};
+int tw_mul(tw_u256* y, const tw_u256* a, const tw_u256* b) {
+ tw_u256 acc = TW_ZERO;
  tw_u256 mul32;
  int overflow = 0;
 
@@ -99,16 +163,179 @@ int tw_mul(tw_u256* y, tw_u256* a, tw_u256* b) {
  return overflow;
 }
 
-void tw_set_256(tw_u256* y, tw_u256* a) {
+int tw_div_rem(tw_u256* y, tw_u256* z, const tw_u256* a, const tw_u256* b) {
+  // Divide by zero
+  if (tw_equal(b, &TW_ZERO)) {
+    return 1;
+  }
+
+  // Numerator is less than denominator
+  if (tw_compare(a, b) < 0) {
+    *y = TW_ZERO;
+    *z = *a;
+    return 0;
+  }
+
+  // Special case for when the denominator is near maximum (2 most significant words
+  // are maximum).  In this case, the exponent for the test divisor would go out
+  // of range when correcting for the overflow of its mantissa.
+  if (b->d[0] == TW_U32_MAX && b->d[1] == TW_U32_MAX) {
+    assert (tw_compare(a, b) >= 0);
+    *y = TW_ONE;
+    int borrow = tw_sub(z, a, b);
+    assert(borrow == 0);
+    return 0;
+  }
+
+  // Initialize remainder and divide accumulator
+  tw_u256 rem = *a;
+  tw_u256 div = TW_ZERO;
+
+  // Scan for MSW of b
+  int b_msw_index = 0;
+  while (b->d[b_msw_index] == 0 && b_msw_index < 7) {
+    b_msw_index++;
+  }
+
+  // The test divisor consists of the top 2 MSWs of the divisor
+  // and an exponent
+  int test_divisor_exp;
+  tw_u64 test_divisor_man;
+
+  if (b_msw_index < 7) {
+    test_divisor_exp = 6 - b_msw_index;
+    test_divisor_man = (((tw_u64) b->d[b_msw_index]) << 32) | ((tw_u64) b->d[b_msw_index + 1]);
+  } else {
+    test_divisor_exp = 0;
+    test_divisor_man = (tw_u64) b->d[b_msw_index];
+  }
+
+  // In order to prevent underflow the test divisor must be greater or equal to the
+  // actual divisor.
+
+  // No change required if exponent is zero, since test_divisor is equal to divisor
+  if (test_divisor_exp != 0) {
+    if (test_divisor_man == TW_U64_MAX) {
+      // If the test_divisor_man is at full scale, incrementing it would cause an overflow
+      // It is right shifted by 32 and the exponent is increased by one.
+      test_divisor_man >>= 32;
+      test_divisor_exp++;
+    }
+
+    // Increment test_divisor_man to ensure the test_divisor is greater
+    // than the actual divisor
+    test_divisor_man++;
+  }
+
+  assert(test_divisor_man != 0);
+  assert(test_divisor_exp < 7);
+
+  #ifndef NDEBUG
+  int loops = 0;
+  #endif
+
+  // Scan for initial MSW of remainder
+  int rem_msw_index = 0;
+  while (rem.d[rem_msw_index] == 0 && rem_msw_index < 6) {
+    rem_msw_index++;
+  }
+
+  // Loop until the remainder is less than the divisor
+  while (tw_compare(&rem, b) >= 0 && rem_msw_index < 7) {
+    #ifndef NDEBUG
+    loops++;
+    assert(loops <= 16);
+    #endif
+
+    // Compute a test remainder for use with the test divisor
+    tw_u64 test_rem_man = (((tw_u64) rem.d[rem_msw_index]) << 32) | rem.d[rem_msw_index + 1];
+    int rem_exp = 6 - rem_msw_index;
+    assert(rem_exp >= 0);
+
+    // Increment the MSW index as the remainder gets smaller, but not
+    // past the 2nd LSW.
+    if (rem_msw_index < 6 && test_rem_man <= TW_U32_MAX) {
+      rem_msw_index++;
+      continue;
+    }
+
+    // Calculate an estimate of the result of the division
+    tw_u64 estimate;
+    int shift = rem_exp - test_divisor_exp;
+
+    // The mantissa for the divisor can be greater than for the remainder in two cases.
+    // -
+    // - The remainder is actually larger but has a lower mantissa
+    if (test_divisor_man > test_rem_man) {
+      if (rem_exp == test_divisor_exp) {
+        // Case 1:  Incrementing the test divisor caused it to be larger than the test
+        //          remainder.  Otherwise, they would have been equal
+        assert(test_divisor_man == test_rem_man + 1);
+        estimate = 1;
+        assert(shift == 0);
+      } else {
+        // Case 2: The remainder has a higher exponent.
+        //         A reduced divisor is used in this case and the shift is adjusted.
+        tw_u64 reduced_divisor = test_divisor_man >> 32;
+        assert(reduced_divisor != TW_U64_MAX);
+        estimate = test_rem_man / (reduced_divisor + 1);
+        shift--;
+      }
+    } else {
+      estimate = test_rem_man / test_divisor_man;
+    }
+
+    assert(shift >= 0);
+    assert(estimate != 0);
+
+    tw_u256 sub;
+
+    int overflow = 0;
+    int borrow = 0;
+    int carry = 0;
+
+    assert(shift < 7);
+
+    // This computes
+    //    remainder = remainder - (estimate * divisor)
+    //    divisor = divisor + estimate
+    //
+    // It is critical that the estimate is lower than the correct value to avoid
+    // underflows.
+
+    // If the estimate cannot be represented in 32-bits, two passes are required
+    if (estimate > 0xFFFFFFFFL) {
+       overflow = tw_mul_32(&sub, b, (tw_u32) (estimate >> 32), shift + 1);
+       borrow = tw_sub(&rem, &rem, &sub);
+       carry = tw_add_32(&div, &div, (tw_u32) (estimate >> 32), shift + 1);
+    }
+
+    overflow |= tw_mul_32(&sub, b, estimate, shift);
+    borrow |= tw_sub(&rem, &rem, &sub);
+    carry |= tw_add_32(&div, &div, estimate, shift);
+
+    assert(estimate != 0);
+
+    assert(!overflow);
+    assert(!borrow);
+    assert(!carry);
+  }
+
+  *y = div;
+  *z = rem;
+
+  return 0;
+}
+
+void tw_set_256(tw_u256* y, const tw_u256* a) {
   for (int i = 0; i < 8; i++) {
     y->d[i] = a->d[i];
   }
 }
 
-void tw_set_32(tw_u256* y, tw_u32 a) {
+void tw_set_32(tw_u256* y, const tw_u32 a) {
   for (int i = 0; i < 7; i++) {
     y->d[i] = 0;
   }
   y->d[7] = a;
 }
-
--- src/tw_u256.h
+++ src/tw_u256.h
@@ -7,58 +7,87 @@
 
 #include <stdint.h>
 
-/* 256-bit unsigned integer data type
- * data[0] is MSW and data[7] is LSW
- */
-typedef struct _tw_u256 {
-  uint32_t d[8];
-} tw_u256;
-
 /* Shorter type names */
 typedef uint32_t tw_u32;
 typedef int32_t tw_i32;
 typedef uint64_t tw_u64;
 typedef int64_t tw_i64;
 
+/* 256-bit unsigned integer data type
+ * data[0] is MSW and data[7] is LSW
+ */
+typedef struct _tw_u256 {
+  tw_u32 d[8];
+} tw_u256;
+
+/* Some constants */
+extern const tw_u256 TW_ZERO;
+extern const tw_u256 TW_ONE;
+
+#define TW_U64_MAX (0xFFFFFFFFFFFFFFFFL)
+#define TW_U32_MAX (0xFFFFFFFF)
+
 /* Equality check
  * Returns 1 if equal
  */
-int tw_equal(tw_u256* a, tw_u256* b);
+int tw_equal(const tw_u256* a, const tw_u256* b);
 
-/* Addition with overflow
- * y = a + b
- * Returns 1 on overflow
+/* Comparison
+ * returns -1 if (a < b), 1 if (a > b) and zero if equal
  */
-int tw_add(tw_u256* y, tw_u256* a, tw_u256* b);
+int tw_compare(const tw_u256* a, const tw_u256* b);
+
+/* Addition with carry
+ * y = a + b
+ * Returns 1 on carry
+ */
+int tw_add(tw_u256* y, const tw_u256* a, const tw_u256* b);
+
+/* Addition by a u32 with word left shift
+ * y = a + (b << (32 * (left_shift & 7)))
+ * Returns 1 on carry
+ */
+int tw_add_32(tw_u256* y, const tw_u256* a, const tw_u32 b, const tw_u32 left_shift);
 
 /* Subtraction with borrow
  * y = a - b
  * Returns 1 on borrow
  */
-int tw_sub(tw_u256* y, tw_u256* a, tw_u256* b);
+int tw_sub(tw_u256* y, const tw_u256* a, const tw_u256* b);
+
+/* Subtraction by a u32 with word left shift
+ * y = a - (b << (32 * (left_shift & 7)))
+ * Returns 1 on borrow
+ */
+int tw_sub_32(tw_u256* y, const tw_u256* a, const tw_u32 b, const tw_u32 left_shift);
 
 /* Multiply by a u32 with word left_shift
- * y = (a * b) << (32 * (left_shift & 7))
+ * y = a * (b << (32 * (left_shift & 7)))
  * Returns 1 on overflow
  */
-int tw_mul_32(tw_u256* y, tw_u256* a, tw_u32 b, tw_u32 left_shift);
+int tw_mul_32(tw_u256* y, const tw_u256* a, const tw_u32 b, const tw_u32 left_shift);
 
 /* Multiply with overflow
  * y = a * b
  * returns 1 on overflow
  */
-int tw_mul(tw_u256* y, tw_u256* a, tw_u256* b);
+int tw_mul(tw_u256* y, const tw_u256* a, const tw_u256* b);
+
+/* Divide and remainder with zero check
+ * y = a / b
+ * z = a % b
+ * returns 1 for divide by zero
+ */
+int tw_div_rem(tw_u256* y, tw_u256* z, const tw_u256* a, const tw_u256* b);
 
 /* Set to a tw_u256
  * y = a
  */
-void tw_set_256(tw_u256* y, tw_u256* a);
-
-int tw_mul(tw_u256* y, tw_u256* a, tw_u256* b);
+void tw_set_256(tw_u256* y, const tw_u256* a);
 
 /* Set to a tw_u32
  * y = (tw_u256) a
  */
-void tw_set_32(tw_u256* y, tw_u32 a);
+void tw_set_32(tw_u256* y, const tw_u32 a);
 
 #endif
